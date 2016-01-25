3e6e5f1f3c245aa16d0b85954e62b4da28933dcc
Daliana Montada Salas <mili.daliana@gmail.com>
2016-01-25 21:22:55 +0100

    Modificación del servidor

--- server.js
+++ server.js
@@ -13,31 +13,31 @@ var server_ip = "85.219.25.246";
 var server_pull_port = 3001;
 //var server_pub_port = 3002;
 
+mongoClient.open(function (err, mongoClient) { //C
+       if (!mongoClient) { //Si hay algun error conectando con mongo
+            console.error("Error! Exiting... No se ha encontrado el servidor");
+            process.exit(1);
+        }
+        console.log("Conexion abierta");
+        // Conexion abierta
+});
 
+var db = mongoClient.db("MyDatabase");
+console.log("Abierta Base de Datos");
 // Socket pull
 var pull_sock = zmq.socket("pull");
 // Conexion
-pull_sock.bind("tcp://" + server_ip + ":" + server_pull_port);
-pull.on("message", function (msg) {
+pull_sock.bind('tcp://*:3001');
+console.log("Escuchando");
+pull_sock.on("message", function (msg) {
     var obj = JSON.parse(msg);
-
-    mongoClient.open(function (err, mongoClient) { //C
-        if (!mongoClient) { //Si hay algun error conectando con mongo
-            console.error("Error! Exiting... No se ha encontrado el servidor de mongoDB");
-            process.exit(1);
-        }
-        var db = mongoClient.db("MyDatabase");  //Me conecto a la DB correcta
-
-
+     console.log("LLEGA: " + msg.toString());
         db.collection('items').insert(obj, function (err, records) {
             if (err) throw err;
             console.log("Añadido item como " + records[0]._id);
             console.log("----------------------------");
         });
 
-    });
-
 });
-
 
 
